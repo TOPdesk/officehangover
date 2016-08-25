9a35f17e3578d3a7895e8e2fcd98674d28dfe412
TIS\RebecaA <r.alonso@topdesk.com>
2016-08-25 09:30:38 +0200

    Added a menu to the game.  Added a intro text to the New Game

--- js/states/HomeState.js
+++ js/states/HomeState.js
@@ -3,42 +3,43 @@
 var RPG = RPG || {};
 
 RPG.HomeState = {
+    init:function(){
+        this.game.Text = this.game.plugins.add(Phaser.Plugin.Text);
+    },
     create: function (){
         var background = this.game.add.sprite(0,0);
         background.width = this.game.world.width;
         background.height = this.game.world.height;
         background.inputEnabled = true;
-        console.log(background);
 
-        var style = {font:'18px Arial', fill: '#000'};
+        var menuPositionY = this.game.world.height/2 - 180;
+        var menuPositionX = this.game.world.centerX/2;
 
-        var text = "You wake up in a large room. There is a lot of sound. You blink a few times. ";
-        text += "You try to determine where you are. Some flashbacks. There was a party. There was beer, tequila. ";
-        text += "An office party. You check your pockets. Phone gone, wallet gone, keys gone. Oh no..... ";
-        text += "Your head hurts, you need coffee and your bed. Let's find your stuff and go home. But first, coffee.. ";
-        var words = text.split(" ");
-        var output = null;
-        var initWidth= 50;
-        var initHeight = 100;
+        var menu = this.game.add.text(menuPositionX, menuPositionY, 'MENU', {'fill': '#FFA500'});
+        var continueGame = this.game.add.text(menuPositionX, menuPositionY+60, 'Continue Game');
+        continueGame.inputEnabled = true;
+        var newGame = this.game.add.text(menuPositionX, menuPositionY+120, 'New Game');
+        newGame.inputEnabled = true;
+        var credits = this.game.add.text(menuPositionX, menuPositionY+180, 'Credits');
 
-        for(var i=0;i<words.length;i++) {
-            var word = words[i];
-            //window.setTimeout(function (word){
-                output = this.game.add.bitmapText(initWidth, initHeight, 'desyrel', word, 30);
-                 if ((output.textWidth + 5 + initWidth)< (background.width - 155)) {
-                     initWidth += output.textWidth + 5;
-                 }
-                 else {
-                     initHeight += output.textHeight + 10;
-                     initWidth = 50;
-                }
-            //}.bind(this, initWidth, initHeight), 2000);
-        }
+        continueGame.events.onInputDown.add(function (){
+            this.state.start(Constants.GAME_STATE);
+        }, this);
 
-        this.game.add.text(this.game.world.centerX + 50, this.game.world.height - 50, 'TOUCH TO START', style);
+        newGame.events.onInputDown.add(function (){
+            menu.destroy();
+            newGame.destroy();
+            continueGame.destroy();
+            credits.destroy();
 
-        background.events.onInputDown.add(function (){
-            this.state.start('GameState');
+            this.textData = JSON.parse(this.game.cache.getText(Constants.GAME_TEXT));
+
+            this.game.Text.setup();
+            
+            this.game.Text.create(this.textData.intro_text, menuPositionX/2, menuPositionY, {}, (function(){
+                this.state.start(Constants.START_STATE);
+            }).bind(this));
+
         }, this);
     }
 };
