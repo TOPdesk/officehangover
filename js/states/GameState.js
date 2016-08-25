f665f452c774e045a0d063f335eea8045e1afcb7
TIS\RebecaA <r.alonso@topdesk.com>
2016-08-25 09:32:17 +0200

    Cleaned the GameState file.

--- js/states/GameState.js
+++ js/states/GameState.js
@@ -10,7 +10,7 @@ RPG.GameState = {
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
 
-        this.gameData = JSON.parse(this.game.cache.getText('constants'));
+        this.playerData = JSON.parse(this.game.cache.getText(Constants.PLAYER_DATA));
 
 
     },
@@ -18,33 +18,25 @@ RPG.GameState = {
 
             this.game.VirtualPad = this.game.plugins.add(Phaser.Plugin.VirtualPad);
 
-            this.game.stage.backgroundColor = this.gameData.background_color;
+            this.game.stage.backgroundColor = this.playerData.background_color;
 
-            this.background = this.game.add.sprite(0, -30, 'background');
+            this.background = this.game.add.sprite(0, -30, Constants.SCENE);
             this.background.width = this.game.world.width + 100;
             this.background.height = this.game.world.height + 100;
             this.background.inputEnabled = true;
             this.game.world.sendToBack(this.background);
 
-            /*this.gameData.initial_position_c1.x = this.game.world.width/6;
-             this.gameData.initial_position_c1.y = this.game.world.height - this.game.world.height/3;
-             this.gameData.initial_position_c2.x = this.game.world.width - this.game.world.width/3;
-             this.gameData.initial_position_c2.y = this.game.world.height/14;*/
-
-            console.log(this.gameData.initial_position_c1.x);
-            console.log(this.gameData.initial_position_c1.y);
-
-            this.player = new RPG.Player(this, this.gameData.initial_position.x, this.gameData.initial_position.y, this.gameData.player_data, 'player', 1);
+            this.player = new RPG.Player(this, this.playerData.initial_position.x, this.playerData.initial_position.y, this.playerData.player, Constants.PLAYER_DATA_INIT, 1);
             this.add.existing(this.player);
             this.player.body.collideWorldBounds = true;
             this.game.camera.follow(this.player);
 
-            this.character1 = new RPG.Player(this, this.gameData.initial_position_c1.x, this.gameData.initial_position_c1.y, this.gameData.player_data, 'character1');
+            this.character1 = new RPG.Player(this, this.playerData.initial_position_c1.x, this.playerData.initial_position_c1.y, this.playerData.player, Constants.CHARACTER1_DATA_INIT);
             this.add.existing(this.character1);
             this.character1.body.collideWorldBounds = true;
 
 
-            this.character2 = new RPG.Player(this, this.gameData.initial_position_c2.x, this.gameData.initial_position_c2.y, this.gameData.player_data, 'character2');
+            this.character2 = new RPG.Player(this, this.playerData.initial_position_c2.x, this.playerData.initial_position_c2.y, this.playerData.player, Constants.CHARACTER2_DATA_INIT);
             this.add.existing(this.character2);
             this.character2.body.collideWorldBounds = true;
 
@@ -76,20 +68,20 @@ RPG.GameState = {
         this.player.body.velocity.y = 0;
 
         if (this.cursors.left.isDown || this.player.btnsPressed.left || this.player.btnsPressed.upleft || this.player.btnsPressed.downleft) {
-            this.player.body.velocity.x = -this.gameData.player_speed;
+            this.player.body.velocity.x = -this.playerData.player_speed;
             this.player.play('walk_left');
         } else if (this.cursors.right.isDown || this.player.btnsPressed.right || this.player.btnsPressed.upright || this.player.btnsPressed.downright) {
-            this.player.body.velocity.x = this.gameData.player_speed;
+            this.player.body.velocity.x = this.playerData.player_speed;
             this.player.play('walk_right');
         }
 
         if (this.cursors.up.isDown || this.player.btnsPressed.up || this.player.btnsPressed.upright || this.player.btnsPressed.upleft) {
-            this.player.body.velocity.y = -this.gameData.player_speed;
+            this.player.body.velocity.y = -this.playerData.player_speed;
             if ((this.cursors.up.isDown || this.player.btnsPressed.up) && (!this.cursors.left.isDown  && !this.cursors.right.isDown)) {
                 this.player.play('walk_up');
             }
         } else if (this.cursors.down.isDown || this.player.btnsPressed.down || this.player.btnsPressed.downright || this.player.btnsPressed.downleft) {
-            this.player.body.velocity.y = this.gameData.player_speed;
+            this.player.body.velocity.y = this.playerData.player_speed;
             if ((this.cursors.down.isDown || this.player.btnsPressed.down) && (!this.cursors.left.isDown  && !this.cursors.right.isDown)) {
                 this.player.play('walk_down');
             }
@@ -101,7 +93,7 @@ RPG.GameState = {
 
         if (this.player.body.velocity.x == 0 && this.player.body.velocity.y == 0) {
             this.player.animations.stop();
-            this.player.frame = this.gameData.initial_frame;
+            this.player.frame = this.playerData.initial_frame;
         }
 
         this.checkCharacter(this.character1);
@@ -134,7 +126,7 @@ RPG.GameState = {
         this.character2.play('walk_right');
     },
     checkCharacter: function (character){
-        if (character.body.position.y > this.game.world.height - 55 && character.body.position.x <= this.gameData.initial_position_c1.x){
+        if (character.body.position.y > this.game.world.height - 55 && character.body.position.x <= this.playerData.initial_position_c1.x){
             character.body.velocity.y = 0;
             character.body.velocity.x = 100;
             character.play('walk_right');
@@ -152,7 +144,7 @@ RPG.GameState = {
             character.play('walk_left');
         }
 
-        if(character.body.position.x <= this.gameData.initial_position_c1.x && character.body.position.y <= this.gameData.initial_position_c1.y){
+        if(character.body.position.x <= this.playerData.initial_position_c1.x && character.body.position.y <= this.playerData.initial_position_c1.y){
             character.body.velocity.y = 100;
             character.body.velocity.x = 0;
             character.play('walk_down');
@@ -171,13 +163,13 @@ RPG.GameState = {
             character.play('walk_left');
         }
 
-        if( character.body.position.x <= this.gameData.initial_position_c2.x && character.body.position.y >= this.gameData.initial_position_c2.y ){
+        if( character.body.position.x <= this.playerData.initial_position_c2.x && character.body.position.y >= this.playerData.initial_position_c2.y ){
             character.body.velocity.y = -50;
             character.body.velocity.x = 0;
             character.play('walk_up');
         }
 
-        if(character.body.position.y <= this.gameData.initial_position_c2.y && character.body.position.x <= this.gameData.initial_position_c2.x){
+        if(character.body.position.y <= this.playerData.initial_position_c2.y && character.body.position.x <= this.playerData.initial_position_c2.x){
             this.character2Movement();
         }
     }
