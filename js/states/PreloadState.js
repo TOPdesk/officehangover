54e1e2bc836245249291f8d5501b692fd6a2a57d
TIS\RebecaA <r.alonso@topdesk.com>
2017-02-15 16:43:17 +0100

    Loaded the new sprite for the character number 3. Changed the name of the principal player sprite needs to be the same that in the tile map

--- js/states/PreloadState.js
+++ js/states/PreloadState.js
@@ -10,6 +10,8 @@ RPG.PreloadState = {
             //params: the reference object, the src for the image, the next two are the size for the frame, total frames in the sheet
             this.load.spritesheet(Constants.CHARACTER1_SPRITE, 'assets/images/character1.png', 64, 64, 273);
             this.load.spritesheet(Constants.CHARACTER2_SPRITE, 'assets/images/character2.png', 64, 64, 273);
+            this.load.spritesheet(Constants.CHARACTER3_SPRITE, 'assets/images/character3.png', 64, 64, 273);
+
             this.load.spritesheet(Constants.PLAYER_SPRITE, 'assets/images/player.png', 64, 64, 273);
             
             this.load.image(Constants.COFFEE_MACHINE_SPRITE, 'assets/images/assets_coffee_machine.png');
