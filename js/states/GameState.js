fec3a49aea03528149e36f1182dc2dff59ee5cbc
Martijn van Iersel <martijni@topdesk.com>
2017-06-09 17:00:06 +0200

    Example code to test animation. Remove me later.

--- js/states/GameState.js
+++ js/states/GameState.js
@@ -133,6 +133,12 @@ RPG.GameState = {
             this.add.existing(this.player);
     	}
 
+        // temporary demo of action cloud
+        var actionCloud = this.game.add.sprite(this.player.x, this.player.y, 'ActionCloud', 0);
+        actionCloud.animations.add('action', [0, 1, 2, 3], 5, true);
+        actionCloud.play('action');
+        // end of demo code
+
     	if (!this.player) {
     		console.error ("No player start position found on map!")
     	}
