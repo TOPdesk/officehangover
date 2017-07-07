c2aefe6fc26d5d898140fad6eee40fbbb818894a
Jalitha Wills <jalithaw@topdesk.com>
2017-07-07 14:00:23 +0200

    Set up Webpack

---
+++ build/webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+const resolve = dir => path.join(__dirname, '..', dir);
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, '../dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				include: [resolve('src')]
+			}
+		]
+	}
+};
