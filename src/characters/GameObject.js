2cc38ab679e1c239b2d22ded3837b0ec30c77543
Rebeca Alonso <rebecaa@topdesk.com>
2017-08-04 10:32:13 +0200

    Remove the dishwasher from the handleCollison function in GameObjects

--- src/characters/GameObject.js
+++ src/characters/GameObject.js
@@ -16,14 +16,10 @@ export default class extends Phaser.Sprite {
 		this.body.setSize(bodySize.width, bodySize.height, bodySize.left, bodySize.top);
 		this.body.immovable = true;
 
-		if(this.key == "dishwasher"){
-			this.alpha = 0;
-		}
-
 	}
 
 	/** called whenever a player collides with this game object */
-	handleCollision(gameObjects) {
+	handleCollision() {
 		if (this.state.justPressedSpace) {
 			if (this.key == "pc" || this.key == "coffeemachine") {
 				this.isExecutingTask = true;
@@ -34,33 +30,9 @@ export default class extends Phaser.Sprite {
 					this.state.player.pickup(this);
 				}
 			}
-		}
-		else if (this.key == "exit") {
+		}else if (this.key == "exit") {
 			this.state.currentLevel = 1;
 			this.state.initLevel();
-		}else if (this.key == "dirtydishes") {
-
-			let dishWasher = gameObjects.filter(function (obj) {
-				return obj.key == 'dishwasher';
-			});
-			dishWasher = dishWasher[0];
-
-			let actionCloud = this.game.add.sprite(this.x, this.y, 'ActionCloud', 0);
-			actionCloud.animations.add('action', [0, 1, 2, 3], 5, true);
-			actionCloud.play('action');
-			actionCloud.alpha = 0;
-
-			let actionCloudDishWasher = this.game.add.sprite(dishWasher.x, dishWasher.y, 'ActionCloud', 0);
-			actionCloudDishWasher.animations.add('action', [0, 1, 2, 3], 5, true);
-			actionCloudDishWasher.play('action');
-
-			this.game.add.tween(this).to( { alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
-			let tween = this.game.add.tween(actionCloud).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true);
-			tween.onComplete.add( function (){actionCloud.destroy();},this);
-
-			this.game.add.tween(dishWasher).to( { alpha: 1 }, 4000, Phaser.Easing.Linear.None, true);
-			this.game.add.tween(actionCloudDishWasher).to( { alpha: 0 }, 4000, Phaser.Easing.Linear.None, true);
-
 		}
 	}
 }
