1e9c317f853ffb62827d49b1bc49256cf4afb425
Martijn van Iersel <mvaniersel@gmail.com>
2018-01-18 12:20:44 +0100

    Compatibility tweak in server.js, add missing dev dependency

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const port = process.env.PORT || 8000;
 const connect = require("connect");
 const serveStatic = require("serve-static");
 
-let serve = serveStatic("./dist");
+var serve = serveStatic("./dist");
 connect().use(serve).listen(port, function() {
 	console.log("Server running on " + port + "...");
 });
